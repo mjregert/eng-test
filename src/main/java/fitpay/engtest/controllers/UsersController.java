96a1436433dfa54b7bd712682cde1c38f5ee2c48
Michael Regert <21973891+mjregert@users.noreply.github.com>
2020-11-08 00:01:28 -0600

    Code backup

--- src/main/java/fitpay/engtest/controllers/UsersController.java
+++ src/main/java/fitpay/engtest/controllers/UsersController.java
@@ -1,14 +1,12 @@
 package fitpay.engtest.controllers;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fitpay.engtest.Application;
-import fitpay.engtest.models.Users;
+import fitpay.engtest.models.User;
 import org.json.JSONArray;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.*;
@@ -16,40 +14,64 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONObject;
 
 @Controller
 public class UsersController extends BaseController {
     @GetMapping("/users")
     @ResponseBody
-    public String getUsers() throws IOException, InterruptedException {
-        String url = String.format("%s/%s", BASEURL, "users"); //"users/137693a0-b4ab-4345-8132-a475930d1a63");
-//        System.out.println("Command = " + command);
-//        BufferedReader reader;
-//
-//        URL url = new URL(command);
-//        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-//        connection.setRequestProperty("Authorization", "Bearer " + Application.oAuthToken);
-//        connection.setDoOutput(true);
-//        connection.setRequestMethod("GET");
-//        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//
-//
-//
-//        String line;
-//        StringBuilder stringBuilder = new StringBuilder();
-//        while ((line = reader.readLine()) != null) {
-//            stringBuilder.append(line);
-//        }
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//
-        //---------------------------------------
+    public List<User> getUsers() throws IOException, InterruptedException {
+        String url = String.format("%s/%s", BASEURL, "users");
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().header("Authorization", "Bearer " + Application.bearerToken).build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Authorization", "Bearer " + Application.bearerToken)
+                .GET()
+                .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JSONObject json = new JSONObject(response.body());
-        JSONArray jArray = json.getJSONArray("results");
-        return null;
+        return getUsers(getResults(response));
+    }
+
+    @GetMapping("/users/{id}")
+    @ResponseBody
+    public User getUser(@PathVariable String id) throws IOException, InterruptedException {
+        String url = String.format("%s/%s/%s", BASEURL, "users", id);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Authorization", "Bearer " + Application.bearerToken)
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return getUser(getResult(response));
+    }
+
+    private JSONArray getResults(HttpResponse<String> response) {
+        // Convert the response to an array of objects
+        JSONObject responseJson = new JSONObject(response.body());
+        return responseJson.getJSONArray("results");
+    }
+
+    private JSONObject getResult(HttpResponse<String> response) {
+        // Convert the response to an array of objects
+        return new JSONObject(response.body());
+    }
+
+    private List<User> getUsers(JSONArray array) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<User> users = new ArrayList<>();
+
+        for (Object user: array) {
+            users.add(getUser((JSONObject)user));
+        }
+        return users;
+    }
+
+    private User getUser(JSONObject json) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json.toString(), User.class);
     }
 }
